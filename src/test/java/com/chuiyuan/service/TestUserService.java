929c7c9f2c21cea8e8bfae50d3646c62a85cb71f
chuiyuan <lisjthu@sina.com>
2016-08-29 10:27:07 +0800

    move applicationContext.xml to classpath

--- src/test/java/com/chuiyuan/service/TestUserService.java
+++ src/test/java/com/chuiyuan/service/TestUserService.java
@@ -5,22 +5,30 @@ import com.chuiyuan.model.User;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by chuiyuan on 16-8-29.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class TestUserService {
-    private static ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-    private static UserService userService = null ;
-    private static User user = null ;
+//    private static ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+//    private static UserService userService = null ;
+//    private static User user = null ;
+    @Autowired
+    private UserService userService;
 
     @BeforeClass
     public static void init(){
-        userService = (UserService)ctx.getBean("userService");
-        user = new User();
-        user.setUserId(112);
+//        userService = (UserService)ctx.getBean("userService");
+//        user = new User();
+//        user.setUserId(112);
     }
 
     @Test
