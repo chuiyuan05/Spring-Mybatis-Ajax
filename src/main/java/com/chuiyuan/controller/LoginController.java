bace03b37fa0b98c72fdafbe9cddc9f7d00715bd
chuiyuan <lisjthu@sina.com>
2016-09-01 17:29:06 +0800

    test xmlhttp controller

--- src/main/java/com/chuiyuan/controller/LoginController.java
+++ src/main/java/com/chuiyuan/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.chuiyuan.controller;
 
+import com.chuiyuan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -10,11 +16,24 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class LoginController {
+    @Autowired
+    UserService userService ;
 
-    @RequestMapping(value = {"/","/login"})
-    public String login(){
-        return "login.jsp";
+    @RequestMapping(value = {"/","/test"})
+    public String test(){
+        return "test.jsp";
     }
 
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public @ResponseBody String login(HttpServletRequest request, HttpServletResponse response){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(userService.hasMatchUser(username, password)){
+            System.out.println("match");
+            return 1+"::"+username;
+        }
+        return "0::error";
+    }
 
 }
