a7ac9565459efbd01f3404054d96461331196daf
chuiyuan <lisjthu@sina.com>
2016-09-06 11:06:04 +0800

    modify User, xml and Mapper

--- src/main/java/com/chuiyuan/domain/User.java
+++ src/main/java/com/chuiyuan/domain/User.java
@@ -7,28 +7,18 @@ import java.util.Date;
  * Created by chuiyuan on 16-8-29.
  */
 public class User implements Serializable{
-    //属性名要和数据库表的字段对应
-    private int id;
-    private String username;// 用户姓名
-    private String password;
-    private String sex;// 性别
-    private Date birthday;// 生日
-    private String address;// 地址
+    private int uid ; //key
+    private String username ;
+    private String passwd ;
+    private String dep ;
+    private String role;
 
-    public String getPassword() {
-        return password;
+    public int getUid() {
+        return uid;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public void setUid(int uid) {
+        this.uid = uid;
     }
 
     public String getUsername() {
@@ -39,34 +29,34 @@ public class User implements Serializable{
         this.username = username;
     }
 
-    public String getSex() {
-        return sex;
+    public String getPasswd() {
+        return passwd;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
     }
 
-    public Date getBirthday() {
-        return birthday;
+    public String getDep() {
+        return dep;
     }
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+    public void setDep(String dep) {
+        this.dep = dep;
     }
 
-    public String getAddress() {
-        return address;
+    public String getRole() {
+        return role;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setRole(String role) {
+        this.role = role;
     }
 
     @Override
     public String toString() {
-        return "User [id=" + id + ", username=" + username +
-                ", password="+password+", sex=" + sex
-                + ", birthday=" + birthday + ", address=" + address + "]";
+        return "User [uid=" + uid + ", username=" + username +
+                ", passwd="+passwd+", dep=" + dep
+                + ", role=" + role + "]";
     }
 }
