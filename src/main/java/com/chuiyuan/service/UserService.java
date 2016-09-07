39d310b429b8281347b9afac512d05ed5bfb4318
chuiyuan <lisjthu@sina.com>
2016-09-07 11:09:07 +0800

    md5 in codeutil

--- src/main/java/com/chuiyuan/service/UserService.java
+++ src/main/java/com/chuiyuan/service/UserService.java
@@ -2,7 +2,7 @@ package com.chuiyuan.service;
 
 import com.chuiyuan.dao.mybatis.UserMapper;
 import com.chuiyuan.domain.User;
-import com.chuiyuan.utils.MD5;
+import com.chuiyuan.utils.CodeUtil;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,7 +25,7 @@ public class UserService {
         logger.info("==>UserService:login");
         Map<String, String> params = new HashMap<String, String>() ;
         params.put("username",username);
-        params.put("passwd", MD5.convert(passwd));
+        params.put("passwd", CodeUtil.createMD5(passwd));
         User user = userMapper.findUserByNameAndPasswd(params);
         if(user != null && user.getUsername().equals(username))
             return true;
--- src/main/java/com/chuiyuan/utils/CodeUtil.java
+++ src/main/java/com/chuiyuan/utils/CodeUtil.java
@@ -1,7 +1,7 @@
 package com.chuiyuan.utils;
 
 import org.apache.log4j.Logger;
-
+import java.security.MessageDigest;
 import java.util.UUID;
 
 /**
@@ -18,5 +18,32 @@ public class CodeUtil {
      */
     public static String createUUID(){
         return UUID.randomUUID().toString().replaceAll("-","").toUpperCase();
+    }
+
+    private static final char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F' };
+
+    public static String createMD5(String input){
+        try {
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(input.getBytes());
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
